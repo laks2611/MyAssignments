f4569f576823da493aaf9c87bdc1ed528ac97057
laks2611 <147971018+laks2611@users.noreply.github.com>
2023-10-15 13:20:45 +0530

    Add files via upload
    Day1

---
+++ day1/Browser.java
@@ -0,0 +1,17 @@
+package week.day1;
+
+public class Browser {
+	public static void main(String[] args) {
+		float browserVersion = 1.1f;
+		String browserName = "chrome";
+				boolean pageLoaded = true;
+		int noOfBrowser = 2;
+		char browserLogo ='C';
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(pageLoaded);
+		System.out.println(noOfBrowser);
+		System.out.println(browserLogo);
+	}
+
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,17 @@
+package week.day1;
+
+public class FindNumber {
+	public static void main(String[] args) {
+		int number = 45;
+		if (number>0) {
+			System.out.println("The number is positive");
+		}
+		else if (number<0) {
+			System.out.println("The number is negative");
+		}
+		else {
+			System.out.println("The number is neutral");
+		}
+	}
+
+}
